480a62a1b276a0ce98b06721a07faa2759ebe05f
Evyatar <evyatar.a@fiverr.com>
2017-08-19 22:52:58 +0300

    update docs

--- stories/index.js
+++ stories/index.js
@@ -24,7 +24,7 @@ function rand(arr) {
 }
 
 function raise() {
-    const toastTimeout = 5000000000;
+    const toastTimeout = 5000;
     ButterToast.raise({
         content: (
             <div className="my-toast">
