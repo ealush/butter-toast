154a09fc1a3739698cf0f9f0a042efe5bb3ec900
Evyatar <evyatar.a@fiverr.com>
2017-08-19 16:02:47 +0300

    allow expanding multi-liners

--- stories/index.js
+++ stories/index.js
@@ -3,10 +3,16 @@ import { storiesOf, action } from '@kadira/storybook';
 import ButterToast from '../src';
 import './style.scss';
 
+const quotes = ["Wubba-lubba-dub-dub!", "Rikki-Tikki-Tavi, biatch!", "And that's why I always say, \"shum-shum-schlippety-dop!\"", "Sometimes science is more art than science, Morty. Lot of people don't get that.", "Quantum carburetor? Jesus Morty, you can't just add a sci-fi word to a car word and hope it means something. Looks like something's wrong with the micro-verse battery.", "Uncertainty is inherently unsustainable. Eventually, everything either is or isn't.", "Oh, shit!", "How is this a fair trial? Our laywer is a Morty.", "You're classified as a hostile entity and Unity doesn't wanna talk to you."];
+
+function randomQuoute() {
+    return quotes[Math.floor(Math.random()*quotes.length)];
+};
+
 function raise() {
     const toastTimeout = Math.round(Math.random()*8000);
     window.dispatchEvent(new CustomEvent('ButterToast', {
-        detail: {text: 'just wanted to say that I really like your shirt!', toastTimeout}
+        detail: {text: randomQuoute(), toastTimeout}
     }));
 }
 
