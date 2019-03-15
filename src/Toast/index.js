b3c7eedfef9331ef96e54a914b1160cb0fd6d168
asafratzon <asaf.ratzon@gmail.com>
2019-03-15 14:49:48 -0500

    [FIX] repeated closing-a-toast bug (#113)

--- src/Toast/index.js
+++ src/Toast/index.js
@@ -66,6 +66,11 @@ class Toast extends Component {
     }
 
     close = () => {
+
+        if (this.state.removed) {
+            return;
+        }
+
         const toastRef = this.toastRef;
         this.clearTimeout();
 
